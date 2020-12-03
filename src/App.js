8990375d982d12fbbf030c66ae18f27f241ef0c9
Chingis Ochirov <chingis09@gmail.com>
2020-12-03 13:46:35 +0800

    controlled inputs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import DateFnsUtils from "@date-io/date-fns"; // choose your lib
 import {
   DatePicker,
@@ -9,10 +9,28 @@ import {
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Slider from "@material-ui/core/Slider";
-import SunCalc from "suncalc";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import useInterval from "@use-it/interval";
 
 import getPosition from "./utils/sun_position";
-import { colors } from "material-ui/styles";
+// import { colors } from "material-ui/styles";
+
+//default value
+const initialState = {};
+
+//reducers
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case action.type === "":
+      return 0;
+    default:
+      return state;
+  }
+};
+
+//store
+const store = createStore(rootReducer, initialState);
 
 const useStyles = makeStyles({
   root: {
@@ -21,21 +39,43 @@ const useStyles = makeStyles({
 });
 
 function valuetext(value) {
-  return `${value}°C`;
+  return `${value}`;
 }
 
 function DiscreteSlider() {
+  // const [date, setDate] = useState(new Date());
+  const currentDate = new Date()
+  const dateO = {
+    year: currentDate.getFullYear(),
+    month: currentDate.getMonth(),
+    date: currentDate.getDate(),
+    hours: currentDate.getHours(),
+    minutes: currentDate.getMinutes(),
+    seconds: currentDate.getSeconds()
+  }
+  const [date, setDate] = useState(dateO);
+  useInterval(() => {
+    setDate(...date, date.setSeconds(date.getSeconds() + 1));
+  }, 1000);
+  const sliderRef = useRef(null)
+  
+  
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <Typography id="discrete-slider" gutterBottom>
         Year
       </Typography>
+      {/* <span>date.</span> */}
       <Slider
-        defaultValue={2020}
+        // defaultValue={2020}
         getAriaValueText={valuetext}
         aria-labelledby="discrete-slider"
         valueLabelDisplay="auto"
+        ref={sliderRef}
+        name="year"
+        value={date.year}
+        onChange={(e, value) => setDate({...date, year: value})}
         step={1}
         marks={false}
         min={1990}
@@ -45,10 +85,13 @@ function DiscreteSlider() {
         Month
       </Typography>
       <Slider
-        defaultValue={1}
+        // defaultValue={1}
         getAriaValueText={valuetext}
         aria-labelledby="discrete-slider"
         valueLabelDisplay="auto"
+        name="month"
+        value={date.month}
+        onChange={(e, value) => setDate({...date, month: value})}
         step={1}
         marks={false}
         min={1}
@@ -62,6 +105,9 @@ function DiscreteSlider() {
         getAriaValueText={valuetext}
         aria-labelledby="discrete-slider"
         valueLabelDisplay="auto"
+        name="day"
+        value={date.date}
+        onChange={(e, value) => setDate({...date, date: value})}
         step={1}
         marks={false}
         min={1}
@@ -75,6 +121,9 @@ function DiscreteSlider() {
         getAriaValueText={valuetext}
         aria-labelledby="discrete-slider"
         valueLabelDisplay="auto"
+        name="hour"
+        value={date.hours}
+        onChange={(e, value) => setDate({...date, hours: value})}
         step={1}
         marks={false}
         min={0}
@@ -88,6 +137,9 @@ function DiscreteSlider() {
         getAriaValueText={valuetext}
         aria-labelledby="discrete-slider"
         valueLabelDisplay="auto"
+        name="minute"
+        value={date.minutes}
+        onChange={(e, value) => setDate({...date, minutes: value})}
         step={1}
         marks={false}
         min={0}
@@ -101,6 +153,9 @@ function DiscreteSlider() {
         getAriaValueText={valuetext}
         aria-labelledby="discrete-slider"
         valueLabelDisplay="auto"
+        name="second"
+        value={date.seconds}
+        onChange={(e, value) => setDate({...date, seconds: value})}
         step={1}
         marks={false}
         min={0}
@@ -114,7 +169,7 @@ function DiscreteSlider() {
 
 function App() {
   const [selectedDate, handleDateChange] = useState(new Date());
-  const { azimuth, altitude } = getPosition(new Date(), 51.84, 0.6);
+  const { azimuth, altitude } = getPosition(new Date(), 51.84, 107.6);
   // var position = SunCalc.getPosition(new Date(), 51, 107);
   const azimuth_to_degrees = (rad) => {
     const degree = rad * (180 / Math.PI);
@@ -145,40 +200,42 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <DatePicker value={selectedDate} onChange={handleDateChange} />
-        <TimePicker value={selectedDate} onChange={handleDateChange} />
-      </MuiPickersUtilsProvider>
+    <Provider store={store}>
+      <div className="App">
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <DatePicker value={selectedDate} onChange={handleDateChange} />
+          <TimePicker value={selectedDate} onChange={handleDateChange} />
+        </MuiPickersUtilsProvider>
 
-      <div className="date">
-        {new Date().toLocaleTimeString("en-US").split("/:" | "/")}
-      </div>
-      <div className="azimuth">Azimuth: {azimuth_deg}</div>
+        <div className="date">
+          {new Date().toLocaleTimeString("en-US").split("/:" | "/")}
+        </div>
+        <div className="azimuth">Azimuth: {azimuth_deg.toFixed(2)}<sup>o</sup></div>
 
-      <div className="altitude">Altitude: {altitude_deg}</div>
-      {/* <div className="altitude">{position.altitude}</div> */}
-      <div
-        className="container"
-        style={{ transform: `rotateZ(${azimuth_deg}deg)` }}
-      >
+        <div className="altitude">Altitude: {altitude_deg.toFixed(2)}<sup>o</sup></div>
+        {/* <div className="altitude">{position.altitude}</div> */}
         <div
-          className="marker"
-          style={{ border: `${color(altitude_deg)} solid 3px` }}
-        ></div>
-      </div>
-      <div
-        className="container"
-        style={{ transform: `rotateZ(${-altitude_deg + 90}deg)` }}
-      >
+          className="container"
+          style={{ transform: `rotateZ(${azimuth_deg}deg)` }}
+        >
+          <div
+            className="marker"
+            style={{ border: `${color(altitude_deg)} solid 3px` }}
+          ></div>
+        </div>
         <div
-          className="marker"
-          style={{ border: `${color(altitude_deg)} solid 3px` }}
-        ></div>
-      </div>
+          className="container"
+          style={{ transform: `rotateZ(${-altitude_deg + 90}deg)` }}
+        >
+          <div
+            className="marker"
+            style={{ border: `${color(altitude_deg)} solid 3px` }}
+          ></div>
+        </div>
 
-      <DiscreteSlider />
-    </div>
+        <DiscreteSlider />
+      </div>
+    </Provider>
   );
 }
 
